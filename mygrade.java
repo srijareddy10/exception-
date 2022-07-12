e0edffb2fc3620067a77857cca436c245d2c9a79
Dhanireddy Srija <srija.d@prodapt.com>
2022-07-12 15:19:09 +0530

    mygrade.java added

--- mygrade.java
+++ mygrade.java
@@ -1,17 +1,18 @@
-package grades;
+package marks;
 import java.io.*;
 import java.util.Scanner;
-
 public class mygrade {
-	public static void main(String[] arg) {
+	static int percentage;
+	static char grade;
+	static String name=null;
+	static int phy=0;
+	static int mat=0;
+	static int chem=0;
+	static int telu=0;
+	static void input() {
 		Scanner sc=new Scanner(System.in);
-		String name=null;
 		System.out.println("enter name of the student : ");
 		name=sc.nextLine();
-		int phy=0;
-		int mat=0;
-		int chem=0;
-		int tel=0;
 		System.out.println("enter the physics marks: ");
 		phy=sc.nextInt();
 		System.out.println("enter the maths marks: ");
@@ -19,13 +20,14 @@ public class mygrade {
 		System.out.println("enter the chemistry marks: ");
 		chem=sc.nextInt();
 		System.out.println("enter the telugu marks: ");
-		tel=sc.nextInt();
+		telu=sc.nextInt();
+		
 		try {
-			int total=(phy+mat+chem+tel);
-			System.out.println(total);
+			int total=(phy+mat+chem+telu);
+			System.out.println("Total: "+total);
 			int percentage=(total/4);
-			System.out.println(percentage);
-			if((percentage>90)&&(percentage<100)) {
+			System.out.println("percentage: "+percentage);
+			if((percentage>90)&&(percentage<=100)) {
 				System.out.println("grade A");
 			}
 			else if(percentage>100) {
@@ -48,9 +50,12 @@ public class mygrade {
 			}
 		}catch(Exception e) {
 			System.out.println(e);
+		}	
 		}
-		
-		
+	public static void main(String[] arg) {
+		input();
+		System.out.println(grade);
 	}
+	}
+	
 
-}
